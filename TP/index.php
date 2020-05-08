581295cb1a69384711fcd293ef368bbdb5b384c6
Tomas Giordano <47353437+tomasgiordano@users.noreply.github.com>
2020-05-08 12:37:45 -0300

    cambiando index

--- TP/index.php
+++ TP/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php 
-        //require_once("./backend/verificarSesion.php");
+        require_once("./backend/verificarSesion.php");
         include("./clases/Fabrica.php");
     ?>
 </head>
